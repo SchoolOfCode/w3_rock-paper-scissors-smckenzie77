722d7c122dbe4d4774a5c1385e484e95f796e022
sellanni <sellanni@hotmail.co.uk>
2021-04-24 00:29:16 +0100

    old-code-plus-neww-code

--- main.js
+++ main.js
@@ -80,3 +80,104 @@ function determineWinner(userChoice, computerChoice) {
 playGame()
 /* before i added in the buttons the code worked fine but
 I cant seem to get the functionality */
+
+
+//all my previous code is below which worked in the console - cannot seem to get it to work with buttons
+/*
+const rockButton = document.querySelector(".rockButton");
+const paperButton = document.querySelector(".paperButton");
+const scissorsButton = document.querySelector(".scissorsButton");
+const guessResult = document.querySelector(".guessResult");
+
+//create functions for select rock/paper/scissors
+
+rockButton.addEventListener("click", selectRock)
+paperButton.addEventListener("click", selectPaper)
+scissorsButton.addEventListener("click", selectScissors)
+
+let userChoice = rockButton|| paperButton || scissorsButton;
+function getUserChoice (userChoice){
+}
+
+function selectRock(){
+  if (userChoice === rockButton && computerChoice === 'rock'){
+    guessResult.innerText = "it\'s a tie!";
+  } else if (userChoice === rockButton && computerChoice === 'paper'){
+    guessResult.innerText = "computer wins"
+  } else (userChoice === rockButton && computerChoice === 'scissors')
+  {
+    guessResult.innerText = "player wins"
+  }
+}
+function selectPaper(){}
+function selectScissors(){}
+
+
+  
+  function getComputerChoice() {
+    let computerChoice = Math.random();
+    if (computerChoice < 0.34) {
+        computerChoice ='rock';
+        return 'rock';
+    } else if(computerChoice <= 0.67) {
+        computerChoice ='paper';
+        return 'paper';
+           
+    } else {
+         computerChoice = 'scissors';
+         computerChoice ='scissors';
+            return 'scissors';
+}
+  }
+  
+ function determineWinner(userChoice,computerChoice) {
+    if (userChoice === computerChoice) {
+      return 'It\'s a tie!';
+    } else if (userChoice === 'rock') {
+      if (computerChoice === 'paper') {
+        return 'Computer wins!';
+      } else {
+        return 'You win!';
+      }
+    } else if (userChoice === 'paper'){
+      if (compterChoice === 'scissors') {
+        return 'Computer wins!';
+      }else {
+        return 'You win!';
+      }
+    } else if (userChoice === 'scissors') {
+      if (computerChoice === 'rock') {
+        return 'Computer wins!';
+      } else {
+        return 'You win!';
+      }
+    } 
+  };
+  
+  function playGame() {
+    let userChoice = getUserChoice();
+    let computerChoice = getComputerChoice();
+    console.log(`you played ${userChoice}`);
+    console.log(`computer played ${computerChoice}`);
+    console.log(determineWinner(userChoice,computerChoice));
+  };
+
+  playGame()
+
+ if (userChoice === "rock" && computerChoice === 'rock'){
+    guessResult.innerText = "it\'s a tie!";
+  } else if (userChoice == rockButton && computerChoice === 'paper'){
+    guessResult.innerText = "computer wins";
+  } else if(userChoice === rockButton && computerChoice === 'scissors'){ 
+    guessResult.innerText = "player wins";
+  }
+
+/* 
+ let userChoice = getUserChoice();
+ let computerChoice = getComputerChoice();
+ console.log(`you played ${userChoice}`);
+ console.log(`computer played ${computerChoice}`);
+ rockButton.addEventListener("click", function(){
+  return userChoice= 'rock';
+})
+*/
